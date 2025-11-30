4fd316e714f09cebf83e9f0bc5bc4be371bd1c1b
Eric Gumba <ericgumba@gmail.com>
2025-11-29 18:55:26 -0800

    added exp field

--- Assets/PlayerStatistics.cs
+++ Assets/PlayerStatistics.cs
@@ -5,6 +5,18 @@ public class PlayerStatistics : MonoBehaviour
     private float interviewChance = .005f;
     private int startingMorale = 100;
     private int moraleStep = 10;
+    private int exp = 0;
+
+    public int Experience
+    {
+        get => exp;
+        set => exp = value;
+    }
+
+    public void AddExperience(int amount)
+    {
+        exp += amount;
+    }
 
     public float InterviewChance => interviewChance;
     public int StartingMorale => startingMorale;
