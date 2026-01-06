6f9ca3602faa5ae9aac6cb5e84ea0fcd9cb8808c
Eric Gumba <ericgumba@gmail.com>
2026-01-05 20:56:14 -0800

    unit tests and new InterviewTracker class

--- Assets/Scripts/Domain/InterviewTracker.cs
+++ Assets/Scripts/Domain/InterviewTracker.cs
@@ -1,17 +1,56 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
-public class InterviewTracker : MonoBehaviour
-{
-    
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+public sealed class InterviewTracker
+{
+    public struct InterviewDate
     {
-        
+        public int Day;
+        public int Hour;
+
+        public InterviewDate(int day, int hour)
+        {
+            Day = day;
+            Hour = hour;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private readonly List<InterviewDate> _interviewDates = new List<InterviewDate>();
+    private readonly TimeDateTracker _timeDateTracker;
+
+    public TimeDateTracker TimeDateTracker => _timeDateTracker;
+
+    public event Action InterviewPopped;
+
+    public InterviewTracker(TimeDateTracker timeDateTracker)
     {
-        
+        _timeDateTracker = timeDateTracker;
+        _timeDateTracker.Changed += OnTimeChanged;
+    }
+
+    public bool TryAddInterviewDate(InterviewDate interviewDate)
+    {
+        if (interviewDate.Day <= _timeDateTracker.Days)
+        {
+            return false;
+        }
+
+        _interviewDates.Add(interviewDate);
+        return true;
+    }
+
+    private void OnTimeChanged()
+    {
+        for (int i = _interviewDates.Count - 1; i >= 0; i--)
+        {
+            var interviewDate = _interviewDates[i];
+            if (interviewDate.Day == _timeDateTracker.Days &&
+                interviewDate.Hour == _timeDateTracker.Hours)
+            {
+                _interviewDates.RemoveAt(i);
+                InterviewPopped?.Invoke();
+            }
+        }
     }
 }
--- Assets/Tests/UnitTests.cs
+++ Assets/Tests/UnitTests.cs
@@ -1,10 +1,71 @@
 using NUnit.Framework;
+using System;
+using System.Collections.Generic;
 
-public class ExampleEditModeTests
+public class InterviewTrackerTests
 {
     [Test]
-    public void Addition_Works()
+    public void TryAddInterviewDateRejectsSameOrPastDay()
     {
-        Assert.AreEqual(4, 2 + 2);
+        var timeDateTracker = new TimeDateTracker(startHour: 8, startDay: 2);
+        var interviewTracker = new InterviewTracker(timeDateTracker);
+
+        var sameDay = new InterviewTracker.InterviewDate(day: 2, hour: 10);
+        var pastDay = new InterviewTracker.InterviewDate(day: 1, hour: 10);
+
+        Assert.IsFalse(interviewTracker.TryAddInterviewDate(sameDay));
+        Assert.IsFalse(interviewTracker.TryAddInterviewDate(pastDay));
+    }
+
+    [Test]
+    public void TryAddInterviewDateAcceptsFutureDay()
+    {
+        var timeDateTracker = new TimeDateTracker(startHour: 8, startDay: 0);
+        var interviewTracker = new InterviewTracker(timeDateTracker);
+
+        var futureDay = new InterviewTracker.InterviewDate(day: 1, hour: 9);
+
+        Assert.IsTrue(interviewTracker.TryAddInterviewDate(futureDay));
+    }
+
+    [Test]
+    public void InterviewPoppedFiresWhenTimeMatchesAndRemovesEntry()
+    {
+        var timeDateTracker = new TimeDateTracker(startHour: 8, startDay: 0);
+        var interviewTracker = new InterviewTracker(timeDateTracker);
+        int fired = 0;
+
+        interviewTracker.InterviewPopped += () => fired++;
+        Assert.IsTrue(interviewTracker.TryAddInterviewDate(new InterviewTracker.InterviewDate(day: 1, hour: 9)));
+
+        for (int i = 0; i < 16; i++)
+        {
+            timeDateTracker.AdvanceTime();
+        }
+
+        Assert.AreEqual(0, fired);
+
+        timeDateTracker.AdvanceTime();
+
+        Assert.AreEqual(1, fired);
+    }
+
+    [Test]
+    public void InterviewPoppedFiresForEachMatchingInterview()
+    {
+        var timeDateTracker = new TimeDateTracker(startHour: 8, startDay: 0);
+        var interviewTracker = new InterviewTracker(timeDateTracker);
+        int fired = 0;
+
+        interviewTracker.InterviewPopped += () => fired++;
+        Assert.IsTrue(interviewTracker.TryAddInterviewDate(new InterviewTracker.InterviewDate(day: 1, hour: 8)));
+        Assert.IsTrue(interviewTracker.TryAddInterviewDate(new InterviewTracker.InterviewDate(day: 1, hour: 8)));
+
+        for (int i = 0; i < 16; i++)
+        {
+            timeDateTracker.AdvanceTime();
+        }
+
+        Assert.AreEqual(2, fired);
     }
 }
